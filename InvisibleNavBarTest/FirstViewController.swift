6ac981d273b136b1851bb00b44e933dc18c4f3d6
Sethmr <sethmr21@gmail.com>
2017-07-17 08:49:13 -0400

    Initial Commit

---
+++ InvisibleNavBarTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  InvisibleNavBarTest
+//
+//  Created by Seth Rininger on 7/17/17.
+//  Copyright © 2017 Vimvest. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
