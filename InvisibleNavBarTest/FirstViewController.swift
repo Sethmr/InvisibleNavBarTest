dc6d7b3b6838a7a50349ab51d6bdcccd377a9a97
Sethmr <sethmr21@gmail.com>
2017-07-17 10:39:02 -0400

    Example Problem

--- InvisibleNavBarTest/FirstViewController.swift
+++ InvisibleNavBarTest/FirstViewController.swift
@@ -12,14 +12,42 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tabBarController?.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
+extension FirstViewController: UITabBarControllerDelegate {
+
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+    
+}
+
+extension FirstViewController: UIViewControllerAnimatedTransitioning {
+
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let _ = transitionContext.viewController(forKey: .from)?.childViewControllers.first as? FirstViewController,
+            let toVC = transitionContext.viewController(forKey: .to)?.childViewControllers.first as? SecondViewController else { return }
+        let containerView = transitionContext.containerView
+
+        containerView.addSubview(toVC.view)
+        containerView.bringSubview(toFront: toVC.view)
+        let viewOriginY = toVC.view.frame.origin.y
+        toVC.view.frame.origin.y = UIScreen.main.bounds.height
+        let duration = transitionDuration(using: transitionContext)
+
+        UIView.animate(withDuration: duration, delay: 0, options: [.transitionCrossDissolve], animations: {
+            toVC.view.frame.origin.y = viewOriginY
+        }, completion: { _ in
+
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        })
+    }
+    
+}
